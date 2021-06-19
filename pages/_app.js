c46c3b33614dcc26420e4bd582fe59d62271dbbf
anup-stha <xxanup.sthaxx@gmail.com>
2021-06-19 22:13:02 +0545

    Initial Website

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import "../styles/globals.css";
+import NextNprogress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextNprogress
+        color="#29D"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
+      <Component {...pageProps} />{" "}
+    </>
+  );
 }
 
 export default MyApp;
